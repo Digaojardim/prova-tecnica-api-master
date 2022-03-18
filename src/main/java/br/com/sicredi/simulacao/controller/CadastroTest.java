5519eb36b749c1d00ea1bd8f1fe4c3938c1f428f
Digão <diogenes.jj@hotmail.com>
2022-03-18 15:44:04 -0300

    Teste API 01

---
+++ src/main/java/br/com/sicredi/simulacao/controller/CadastroTest.java
@@ -0,0 +1,44 @@
+package br.com.sicredi.simulacao.controller;
+
+import antlr.build.Tool;
+import io.restassured.http.ContentType;
+import org.junit.Test;
+import static io.restassured.RestAssured.*;
+import static io.restassured.matcher.RestAssuredMatchers.*;
+import static org.hamcrest.Matchers.*;
+
+public class CadastroTest {
+    @Test
+    public void testDadoQueCadastroUsuarioQuandoPreenchoAsiInformacoesEntaoValidoStatusCode200(){
+        // Configurar o caminho comum de acesso a minha API Rest
+        baseURI = "http://localhost";
+        port = 8888;
+        basePath ="/api";
+
+        // Cadastrar usuario
+        given()
+                .body("{\n" +
+                        "  \"nome\": \"papito\",\n" +
+                        "  \"cpf\": 97093236014,\n" +
+                        "  \"email\": \"papito@email.com\",\n" +
+                        "  \"valor\": 7200,\n" +
+                        "  \"parcelas\": 3,\n" +
+                        "  \"seguro\": true\n" +
+                        "}")
+                .contentType(ContentType.JSON)
+                .when()
+                .post("/v1/simulacoes")
+                .then()
+                .log().all()
+
+                     .statusCode(200);
+
+
+    }
+
+
+
+
+}
+
+
