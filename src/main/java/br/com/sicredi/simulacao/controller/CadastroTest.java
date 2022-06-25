c50ae7186d935b87fca5ec99391726469c662c2b
Diógenes Jardim <97700904+Digaojardim@users.noreply.github.com>
2022-06-25 16:14:27 -0300

    Merge remote-tracking branch 'origin/master'

--- src/main/java/br/com/sicredi/simulacao/controller/CadastroTest.java
+++ src/main/java/br/com/sicredi/simulacao/controller/CadastroTest.java
@@ -9,34 +9,32 @@ import static org.hamcrest.Matchers.*;
 
 public class CadastroTest {
     @Test
-    public void testDadoQueCadastroUsuarioQuandoPreenchoAsiInformacoesEntaoValidoStatusCode200(){
+    public void testDadoQueCadastroUsuarioQuandoPreenchoAsiInformacoesEntaoValidoStatusCode201() {
         // Configurar o caminho comum de acesso a minha API Rest
         baseURI = "http://localhost";
-        port = 8888;
-        basePath ="/api";
+        port = 8080;
+        basePath = "/api";
 
         // Cadastrar usuario
         given()
                 .body("{\n" +
                         "  \"nome\": \"papito\",\n" +
-                        "  \"cpf\": 97093236014,\n" +
+                        "  \"cpf\": 97093236093,\n" +
                         "  \"email\": \"papito@email.com\",\n" +
                         "  \"valor\": 7200,\n" +
                         "  \"parcelas\": 3,\n" +
                         "  \"seguro\": true\n" +
                         "}")
-                .contentType(ContentType.JSON)
+                      .contentType(ContentType.JSON)
                 .when()
-                .post("/v1/simulacoes")
+                      .post("/v1/simulacoes")
                 .then()
-                .log().all()
-
-                     .statusCode(200);
+                      .log().all()
+                      .assertThat()
+                          .statusCode(201);
 
 
     }
-
-
 
 
 }
