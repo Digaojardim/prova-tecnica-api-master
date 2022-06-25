1bc615734469be79328320c4f43b5967d1124c4a
Diógenes Jardim <97700904+Digaojardim@users.noreply.github.com>
2022-06-25 16:26:37 -0300

    Merge remote-tracking branch 'origin/master'

--- src/main/java/br/com/sicredi/simulacao/controller/CadastroTest.java
+++ src/main/java/br/com/sicredi/simulacao/controller/CadastroTest.java
@@ -25,18 +25,124 @@ public class CadastroTest {
                         "  \"parcelas\": 3,\n" +
                         "  \"seguro\": true\n" +
                         "}")
-                      .contentType(ContentType.JSON)
+                .contentType(ContentType.JSON)
                 .when()
-                      .post("/v1/simulacoes")
+                .post("/v1/simulacoes")
                 .then()
-                      .log().all()
-                      .assertThat()
-                          .statusCode(201);
+                .log().all()
+                .assertThat()
+                .statusCode(201);
 
 
     }
 
+    @Test
+    public void testDadoQueCadastroUsuario01QuandoPreenchoAsiInformacoesEntaoValidoStatusCode201() {
+        // Configurar o caminho comum de acesso a minha API Rest
+        baseURI = "http://localhost";
+        port = 8080;
+        basePath = "/api";
 
+        // Cadastrar usuario
+        given()
+                .body("{\n" +
+                        "  \"nome\": \"joao\",\n" +
+                        "  \"cpf\": 47093236093,\n" +
+                        "  \"email\": \"papito@email.com\",\n" +
+                        "  \"valor\": 2200,\n" +
+                        "  \"parcelas\": 3,\n" +
+                        "  \"seguro\": true\n" +
+                        "}")
+                .contentType(ContentType.JSON)
+                .when()
+                .post("/v1/simulacoes")
+                .then()
+                .log().all()
+                .assertThat()
+                .statusCode(201);
+
+
+    }
+
+    @Test
+    public void testDadoQueCadastroUsuario02QuandoPreenchoAsiInformacoesEntaoValidoStatusCode201() {
+        // Configurar o caminho comum de acesso a minha API Rest
+        baseURI = "http://localhost";
+        port = 8080;
+        basePath = "/api";
+
+        // Cadastrar usuario
+        given()
+                .body("{\n" +
+                        "  \"nome\": \"Pedro\",\n" +
+                        "  \"cpf\": 45093236093,\n" +
+                        "  \"email\": \"pedro@email.com\",\n" +
+                        "  \"valor\": 8200,\n" +
+                        "  \"parcelas\": 3,\n" +
+                        "  \"seguro\": true\n" +
+                        "}")
+                .contentType(ContentType.JSON)
+                .when()
+                .post("/v1/simulacoes")
+                .then()
+                .log().all()
+                .assertThat()
+                .statusCode(201);
+
+    }
+
+    @Test
+    public void testDadoQueCadastroUsuario04QuandoPreenchoAsiInformacoesEntaoValidoStatusCode201() {
+        // Configurar o caminho comum de acesso a minha API Rest
+        baseURI = "http://localhost";
+        port = 8080;
+        basePath = "/api";
+
+        // Cadastrar usuario
+        given()
+                .body("{\n" +
+                        "  \"nome\": \"Gabril\",\n" +
+                        "  \"cpf\": 34093236093,\n" +
+                        "  \"email\": \"biel@email.com\",\n" +
+                        "  \"valor\": 6700,\n" +
+                        "  \"parcelas\": 3,\n" +
+                        "  \"seguro\": true\n" +
+                        "}")
+                .contentType(ContentType.JSON)
+                .when()
+                .post("/v1/simulacoes")
+                .then()
+                .log().all()
+                .assertThat()
+                .statusCode(201);
+
+    }
+
+    @Test
+    public void testDadoQueCadastroUsuario05QuandoPreenchoAsiInformacoesEntaoValidoStatusCode201() {
+        // Configurar o caminho comum de acesso a minha API Rest
+        baseURI = "http://localhost";
+        port = 8080;
+        basePath = "/api";
+
+        // Cadastrar usuario
+        given()
+                .body("{\n" +
+                        "  \"nome\": \"Araujo\",\n" +
+                        "  \"cpf\": 83493236093,\n" +
+                        "  \"email\": \"araujo@email.com\",\n" +
+                        "  \"valor\": 7200,\n" +
+                        "  \"parcelas\": 3,\n" +
+                        "  \"seguro\": true\n" +
+                        "}")
+                .contentType(ContentType.JSON)
+                .when()
+                .post("/v1/simulacoes")
+                .then()
+                .log().all()
+                .assertThat()
+                .statusCode(201);
+
+    }
 }
-
 
